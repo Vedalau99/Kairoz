edcdbba06139ee659f0d1f6d85e9315bb0dade43
Vedalau99 <uma.vedala09@gmail.com>
2025-08-05 15:43:57 +0530

    initial commit

--- kairoz-pipeline.groovy
+++ kairoz-pipeline.groovy
@@ -1,40 +1,30 @@
-<?xml version='1.1' encoding='UTF-8'?>
-<flow-definition plugin="workflow-job">
-  <description>Jenkins CI/CD for Kairoz</description>
-  <keepDependencies>false</keepDependencies>
-  <properties/>
-  <definition class="org.jenkinsci.plugins.workflow.cps.CpsFlowDefinition" plugin="workflow-cps">
-    <script>
-      pipeline {
-          agent any
+pipeline {
+  agent any
 
-          stages {
-              stage('Build Web Image') {
-                  steps {
-                      sh 'docker build -t kairoz-web ./web'
-                  }
-              }
-              stage('Build Worker Image') {
-                  steps {
-                      sh 'docker build -t kairoz-worker ./worker'
-                  }
-              }
-              stage('Load to Minikube') {
-                  steps {
-                      sh 'minikube image load kairoz-web'
-                      sh 'minikube image load kairoz-worker'
-                  }
-              }
-              stage('Deploy to Kubernetes') {
-                  steps {
-                      sh 'kubectl apply -f ./k8s'
-                  }
-              }
-          }
+  stages {
+    stage('Build Web Image') {
+      steps {
+        sh 'docker build -t kairoz-web ./web'
       }
-    </script>
-    <sandbox>true</sandbox>
-  </definition>
-  <triggers/>
-  <disabled>false</disabled>
-</flow-definition>
+    }
+
+    stage('Build Worker Image') {
+      steps {
+        sh 'docker build -t kairoz-worker ./worker'
+      }
+    }
+
+    stage('Load Images to Minikube') {
+      steps {
+        sh 'minikube image load kairoz-web'
+        sh 'minikube image load kairoz-worker'
+      }
+    }
+
+    stage('Deploy to Kubernetes') {
+      steps {
+        sh 'kubectl apply -f k8s/'
+      }
+    }
+  }
+}
